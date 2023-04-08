a46bae23eaad38a51bfe3742e67947515b8cd991
AndersonPm-HUB <anderson.pedroza@uniminuto.edu.co>
2023-04-08 18:31:31 -0500

    inicializacion del proyecto

---
+++ Frontend/js/menu.js
@@ -0,0 +1,31 @@
+const menu = `
+<nav>
+<ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#project">Projects</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">About</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contact</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Blog</a>
+  </li>
+</ul>
+</nav>
+`;
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('menu').innerHTML = menu;
+  });
+  
+  
+  
+  
+  
+  
